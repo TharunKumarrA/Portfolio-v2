ec5bdcd6a600989d1e6fd15f59161cb0f44036aa
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-24 01:43:41 +0530

    Underlined head text component

---
+++ src/app/components/UnderlinedText.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+const TextUnderlineEffect = ({ text }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const scale = isMobile ? 1.15 : 2;
+
+  return (
+    <div className="relative inline-block">
+      <span className="relative text-h5 md:text-h3 font-heading z-20">
+        {text}
+      </span>
+      <div
+        className="absolute bottom-2 left-0 h-1 md:h-2 bg-red-500 transition-width duration-500 ease-in-out z-10"
+        style={{ width: `${text.length * scale}rem` }}
+      />
+    </div>
+  );
+};
+
+export default TextUnderlineEffect;
