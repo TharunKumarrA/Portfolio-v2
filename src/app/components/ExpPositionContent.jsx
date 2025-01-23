de4ff61b94affa44deb624bcef39fc48aa53fc81
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-24 02:00:18 +0530

    Experience & Position content component

---
+++ src/app/components/ExpPositionContent.jsx
@@ -0,0 +1,23 @@
+import TextUnderlineEffect from "./UnderlinedText";
+
+const ExpPositionContent = ({ role, company, details }) => {
+  return (
+    <div className="flex flex-col gap-4 px-8 md:px-4">
+      <div className="flex flex-col gap-2">
+        {role.map((role, index) => (
+          <div key={index}>
+            <TextUnderlineEffect text={role} />
+          </div>
+        ))}
+      </div>
+      <div className="text-h6 md:text-h5 font-heading text-text">
+        {company}
+      </div>
+      <div className="text-p font-body text-text max-w-lg">
+        {details}
+      </div>
+    </div>
+  );
+};
+
+export default ExpPositionContent;
