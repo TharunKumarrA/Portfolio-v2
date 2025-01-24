8d0da379c9537fefd1fd019d58edd9249c5db524
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-24 13:58:52 +0530

    Profile Image component

---
+++ src/app/components/ProfileImage.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import CosmosLogo from "./svg/CosmosLogo";
+
+const ProfileImage = ({ imgSrc, altText }) => {
+  return (
+    <div className="relative p-6 sm:p-0">
+      <Image
+        src={imgSrc}
+        alt={altText}
+        width={480}
+        height={480}
+        className="rounded-full w-full max-w-[320px] mx-auto"
+      />
+      <div className="absolute bottom-0 -left-10 md:bottom-2 md:left-2 transform scale-50 md:scale-75">
+        <CosmosLogo />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileImage;
