68e967a749afdfa9b51a3692b88a7dfc791245ec
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-27 03:21:32 +0530

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
-      if (!isServer) {
-          // Prevent Webpack from trying to resolve 'canvas' during client-side builds
-          config.resolve.alias['canvas'] = false;
-      }
-      return config;
+    if (!isServer) {
+      // Prevent Webpack from trying to resolve 'canvas' during client-side builds
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      };
+    }
+    return config;
   },
 };
 
