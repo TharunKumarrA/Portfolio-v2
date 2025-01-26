23d9f1a64ecdc8c77cc15f88e2fb21c95e24c1d6
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-27 03:09:36 +0530

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Add fallback for 'canvas' to prevent build errors
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
