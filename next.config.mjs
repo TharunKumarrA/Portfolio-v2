d8ad622091303750715e71bdf7b929e52833d937
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-27 03:33:03 +0530

    revert of fix ... CRYYYYYYYY

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Prevent Webpack from trying to resolve 'canvas' during client-side builds
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        canvas: false,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
