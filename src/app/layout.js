f9affa80b296e8b8bc48cf35c9c38d646f73e20e
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-27 22:01:49 +0530

    meta data modifications

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Space_Mono, Rubik } from "next/font/google";
 import "./globals.css";
 import { NavigationBar } from "./components/NavigationBar";
-import { RouteImage } from "./components/RouteImage"; 
+import { RouteImage } from "./components/RouteImage";
 import { Footer } from "./components/footer";
 
 const spaceMono = Space_Mono({
@@ -17,12 +17,41 @@ const rubik = Rubik({
 });
 
 export const metadata = {
-  title: "Tharun's Portfolio",
-  description: "Portfolio website of Tharun Kumarr A",
+  title: "Tharun Kumarr A",
+  description:
+    "Official portfolio of Tharun Kumarr A - Frontend Developer and Tech Enthusiast.",
+  keywords:
+    "Tharun Kumarr, Portfolio, Frontend Developer, Web Developer, Tech Enthusiast",
+  author: "Tharun Kumarr A",
   icons: {
-    icon: '/cosmos-logo.ico',
-    apple: '/cosmos-logo.ico',
-    shortcut: '/cosmos-logo.ico',
+    icon: "/cosmos-logo.ico",
+    apple: "/cosmos-logo.ico",
+    shortcut: "/cosmos-logo.ico",
+  },
+  themeColor: "#D8392B",
+  openGraph: {
+    type: "website",
+    title: "Tharun Kumarr A",
+    description:
+      "Explore Tharun Kumarr's journey in frontend development and technology.",
+    url: "https://tharunkumarra.vercel.app",
+    images: [
+      {
+        url: "/banner.png", // LinkedIn preview image
+        width: 1200,
+        height: 630,
+        alt: "Tharun Kumarr Portfolio Banner",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@astro_tharun",
+    creator: "@astro_tharun",
+    title: "Tharun Kumarr A",
+    description:
+      "Explore Tharun Kumarr's journey in frontend development and technology.",
+    images: ["/banner.png"],
   },
 };
 
