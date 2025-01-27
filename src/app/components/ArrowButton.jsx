9a2a6af4a4e2f4e2815aba079d7f33aa807a4b41
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-27 22:29:36 +0530

    grid border update

--- src/app/components/ArrowButton.jsx
+++ src/app/components/ArrowButton.jsx
@@ -12,7 +12,7 @@ const DownArrowBox = () => {
   return (
     <div
       onClick={handleClick}
-      className="w-12 h-12 lg:w-24 lg:h-24 border-text border-2 bg-primary flex justify-center items-center cursor-pointer hover:bg-[#E96B5C] hover:border-opacity-80 transition-colors duration-200 active:bg-primary active:border-opacity-80"
+      className="w-12 h-12 lg:w-24 lg:h-24 border-text border-opacity-30 border-2 bg-primary flex justify-center items-center cursor-pointer hover:bg-[#E96B5C] hover:border-opacity-80 transition-colors duration-200 active:bg-primary active:border-opacity-80"
     >
       <FiArrowDown className="text-black text-h5 lg:text-h3 hover:text-opacity-80" />
     </div>
